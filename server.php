b36930c4aac7349ac4250e57329854f785d05ebf
Nijji <alwinsenior@outlook.com>
2022-02-19 10:03:15 +0300

    updated auth

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author    alwin abok <alwin@laravel.com>
  */
 
 $uri = urldecode(
